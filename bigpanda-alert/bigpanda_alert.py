51ba937902ae937f35c46732e0b3b571cf2194d2
Hagai Kariti <hagai@bigpanda.io>
2015-03-31 11:12:44 +0300

    1.3

--- bigpanda-alert/bigpanda_alert.py
+++ bigpanda-alert/bigpanda_alert.py
@@ -14,7 +14,7 @@ except ImportError:
     import simplejson as json
 
 
-VERSION='1.2'
+VERSION='1.3'
 
 current_dir = os.path.realpath(os.path.dirname(__file__))
 CONFIG_FILE = os.path.join(current_dir, 'config.ini')
